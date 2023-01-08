46b809b8243246ab5cd3d201d51fc321bdbe40d3
Oblitus <piotr5jurczak@gmail.com>
2023-01-08 14:23:01 +0100

    add cards

---
+++ Lab7/core.js
@@ -0,0 +1,20 @@
+const ctx = document.querySelector('#chart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Pon', 'Wt', 'Śr', 'Czw', 'Pt', 'Sob', 'Niedz'],
+        datasets: [{
+            label: 'Stopnie (C)',
+            data: [12, 19, 3, 5, 2, 12, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
